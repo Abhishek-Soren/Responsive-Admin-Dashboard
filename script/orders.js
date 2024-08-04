269ecab9b74dfb406d667910d6994d9149fb86d2
Abhishek Soren <abhisheksoren95@gmail.com>
2024-08-05 05:12:48 +0530

    update

---
+++ script/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: "Overgeared Manhwa",
+        productNumber: "120954",
+        paymentStatus: "Due",
+        status: "Pending",
+    },
+    {
+        productName: "Fairy Tail Manga",
+        productNumber: "786392",
+        paymentStatus: "Refunded",
+        status: "Declined",
+    },
+    {
+        productName: "Frieren Manga",
+        productNumber: "234944",
+        paymentStatus: "Paid",
+        status: "Active",
+    },
+];
